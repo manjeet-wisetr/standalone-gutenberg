8ea51722c4178feb8c6679811705b9452805fa05
manjeet-wisetr <manjeet.k@wisetr.com>
2022-01-18 21:07:05 +0530

    file added

---
+++ .babelrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    plugins: [
+        ['@wordpress/babel-plugin-import-jsx-pragma', {
+            scopeVariable: 'createElement',
+            scopeVariableFrag: 'Fragment',
+            source: '@wordpress/element',
+            isDefault: false,
+        }],
+        ['@babel/plugin-transform-react-jsx', {
+            pragma: 'createElement',
+            pragmaFrag: 'Fragment',
+        }],
+        ["@babel/plugin-syntax-dynamic-import"]
+    ],
+};
